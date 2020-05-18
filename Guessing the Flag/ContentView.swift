63e4eca9b6daa04532eebd86e22cf2d756e44b2d
Weixi779 <1349750521@qq.com>
2020-05-18 19:58:35 +0800

    Initial Commit

---
+++ Guessing the Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guessing the Flag
+//
+//  Created by 孙世伟 on 2020/5/18.
+//  Copyright © 2020 me.Weixi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
